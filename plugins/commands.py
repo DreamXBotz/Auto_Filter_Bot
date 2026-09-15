3914c9dbce83bd64c5bda8b7eecfaecf8b3e3b9e
ab9816892-dot <ab9816892@gmail.com>
2026-09-15 22:09:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -139,7 +139,6 @@ async def start(client, message):
                     
                     if f_caption is None:
                         f_caption = f"{clean_filename(files1.file_name)}"
-                        
                     sent_msg = await client.send_cached_media(
                         chat_id=message.from_user.id,
                         cover=cover,
@@ -175,7 +174,6 @@ async def start(client, message):
                     cover=cover,
                     caption=f_caption,
                     protect_content=settings.get('file_secure', PROTECT_CONTENT),
-                    reply_markup=InlineKeyboardMarkup(btn)
                 )
                 filesarr.append(sent_msg)
 
